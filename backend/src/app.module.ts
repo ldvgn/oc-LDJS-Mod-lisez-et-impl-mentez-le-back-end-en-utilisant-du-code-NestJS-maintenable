ad74c38142a238ec2738ac4bb82bbad0f15c54ce
Laury <l.devraigne@gmail.com>
2026-08-25 21:21:56 +0200

    feat(messages): add protected POST /messages endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RentalsModule } from './rentals/rentals.module';
+import { MessagesModule } from './messages/messages.module';
 @Module({
   imports: [
     PrismaModule,
@@ -11,6 +12,7 @@ import { RentalsModule } from './rentals/rentals.module';
     AuthModule,
     UsersModule,
     RentalsModule,
+    MessagesModule,
   ],
   controllers: [],
   providers: [],
